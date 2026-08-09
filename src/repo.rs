5b85e859918fb8c157032793dde402e370ad7610
Janorovic Volkov 🦊🌸 <janorovicvolkov@gmail.com>
2026-08-09 21:45:28 +0700

    Update repo.rs

--- src/repo.rs
+++ src/repo.rs
@@ -437,10 +437,12 @@ fn find_pkg_in_json_repos(
 pub fn download_pkg_url(
     cfg: &Config, 
     url: &str, 
-    pb: Option<indicatif::ProgressBar>
+    pb: Option<indicatif::ProgressBar>,
+    overall_pb: Option<indicatif::ProgressBar>,
 ) -> Result<PathBuf, LkpmError> {
     let file_name = package_file_name_from_url(url);
     let local = cfg.cache_path.join(&file_name);
     fs::create_dir_all(&cfg.cache_path).map_err(LkpmError::Io)?;
-    download_to(url, &local, pb.as_ref()).map_err(|e| LkpmError::Network(e.to_string()))
+    downloader::download_to(url, &local, pb.as_ref(), overall_pb.as_ref())
+        .map_err(|e| LkpmError::Network(e.to_string()))
 }
