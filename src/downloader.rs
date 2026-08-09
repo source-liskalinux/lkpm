c0f7487da0d1c155ad8b25d1baba02a80f0ea859
Janorovic Volkov 🦊🌸 <janorovicvolkov@gmail.com>
2026-08-10 03:39:29 +0700

    Update downloader.rs

--- src/downloader.rs
+++ src/downloader.rs
@@ -2,6 +2,7 @@ use anyhow::{Context, Result};
 use reqwest::blocking::Client;
 use std::collections::VecDeque;
 use std::fs;
+use std::fs::File;
 use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
 use std::sync::{mpsc, Arc, Mutex};
